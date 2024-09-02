c88c00b06472ebdb279972a1fd80830a9a17016f
ecourse <smkang@codenuri.co.kr>
2024-09-02 14:15:17 +0900

    Add files via upload

---
+++ DAY1/5_함수템플릿2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+// 아래 코드를 보고 ? 채워 보세요
+template<typename T1, typename T2>
+? Add(T1 a, T2 b)
+{
+	return a + b;
+}
+
+int main()
+{
+	std::cout << Add(1.1, 2) << std::endl; 
+
+}
+
