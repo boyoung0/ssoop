fce3bc9629244c357d37a7736f7d753f7b6c1990
ecourse <smkang@codenuri.co.kr>
2024-09-02 13:03:48 +0900

    Add files via upload

---
+++ DAY1/4_함수2.cpp
@@ -0,0 +1,23 @@
+﻿// C++ 함수 특징 2. inline   .. 32page
+
+
+       int add1(int a, int b) { return a + b; }
+inline int add2(int a, int b) { return a + b; }
+
+int main()
+{
+	int n1 = add1(1, 2);	// 1. 인자를 약속된 장소에 넣고			
+							// 2. 함수로 이동
+							// 32bit		64bit
+							// push  2		mov edi, 2
+							// push  1		mov sdi, 1
+							// call add1	call add1
+
+	int n2 = add2(1, 2);	// add2 의 기계어 코드를
+							// 이 위치에 치환해달라.
+							// mov eax, 2
+							// add eax, 1
+							// mov n, eax
+}
+
+
