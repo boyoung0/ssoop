c2b5c3deb8765b309ed5b0cd233dcc4b1dc3ae09
ecourse <smkang@codenuri.co.kr>
2024-09-02 09:57:06 +0900

    Add files via upload

---
+++ DAY1/1_namespace4.cpp
@@ -0,0 +1,20 @@
+﻿// 10 page 내용입니다.
+
+//#include <stdio.h>  // printf 가 global 에 있습니다
+
+#include <cstdio> // printf 가 global 에도 있고
+				  // std 안에도 있습니다.
+
+// C 용헤더		C++ 에서 새로만든 헤더
+// <xxx.h> =====> <cxxx>
+// <stdlib.h> ==> < ? >
+// <string.h> ==> < ? >
+
+
+int main()
+{
+	// printf 는 C 표준이지만 C++ 표준이기도 합니다.
+	printf("hello\n"); // ok
+
+	std::printf("hello\n"); // ??
+}
