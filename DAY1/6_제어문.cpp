36da1cd2ba2c06930d9b73c55f3b4eff0c7aa48c
ecourse <smkang@codenuri.co.kr>
2024-09-02 14:43:22 +0900

    Add files via upload

--- DAY1/6_제어문.cpp
+++ DAY1/6_제어문.cpp
@@ -10,8 +10,15 @@ int main()
 
 	// 위 코드를 아래 처럼 표현할수 있습니다.
 	// ++17 부터, "초기화 구문을 가지는 제어문"
-	if (int ret = foo(); ret == 10)
+	if ( int ret2 = foo(); ret2 == 10 )
 	{
 
+	} // <= 여기서 ret2 파괴.
+
+	// switch 도 가능.
+	switch (int n = foo(); n)
+	{
+	case 1: break;
+	case 10: break;
 	}
 }
