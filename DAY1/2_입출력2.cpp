b5d4146beac1c057a19034a3bc072a40c0e01453
ecourse <smkang@codenuri.co.kr>
2024-09-02 10:10:01 +0900

    Add files via upload

---
+++ DAY1/2_입출력2.cpp
@@ -0,0 +1,14 @@
+﻿// 3_입출력2.cpp - 18 page아래 부분
+#include <iostream>
+
+int main()
+{
+	int n = 10;
+	std::cout << n << std::endl; // 10.  10진수로 출력
+
+	std::cout << std::hex << n << std::endl; // a, 16진수 출력
+
+
+	std::cout << n << std::endl; // 
+	std::cout << n << std::endl; // 
+}
