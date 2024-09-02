6ead6677f3395c9ab10f9ee55a55161104b4f677
ecourse <smkang@codenuri.co.kr>
2024-09-02 13:14:34 +0900

    Add files via upload

---
+++ DAY1/4_함수3.cpp
@@ -0,0 +1,22 @@
+﻿#include <cstdio>
+
+
+// 37 page
+// overloading
+// => 인자의 갯수나, 타입이 다르면 함수이름동일한 함수를
+//    여러개 만들수 있다.
+int square(int a)
+{
+	return a * a;
+}
+
+double square(double a)
+{
+	return a * a;
+}
+
+int main()
+{
+	square(3);
+	square(3.4);
+}
