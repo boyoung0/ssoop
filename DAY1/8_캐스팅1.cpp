5f55bb8a29d804e17c299ccb086c8a7c89f32a18
ecourse <smkang@codenuri.co.kr>
2024-09-02 15:21:49 +0900

    Add files via upload

---
+++ DAY1/8_캐스팅1.cpp
@@ -0,0 +1,22 @@
+﻿// 10_캐스팅1.cpp    49 page
+#include <iostream>
+
+int main()
+{
+	int n = 3;
+
+	// #1. 포인터와 명시적 캐스팅
+	int*  p1 = &n;	// ok
+//	char* p2 = &n;	// C언어 ok. C++ 에러. 즉, 암시적 형변환 안됨
+	char* p2 = (char*)&n; // C++ 이라도 ok.
+
+	// #2. 왜 int 변수의 주소를 char* 에 담는가?
+	// 의도는 무엇인가 ?
+	int n2 = 0x11223344;
+	int* p3 = &n2;
+	char* p4 = (char*)& n2;
+
+	printf("%x\n", *p3); // 11223344
+	printf("%x\n", *p4); // 44
+
+}
