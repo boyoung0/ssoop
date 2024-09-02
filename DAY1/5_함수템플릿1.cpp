52a4e8edb58f5f3bee3748fc6ffd9edd773e9204
ecourse <smkang@codenuri.co.kr>
2024-09-02 14:18:19 +0900

    Add files via upload

--- DAY1/5_함수템플릿1.cpp
+++ DAY1/5_함수템플릿1.cpp
@@ -5,10 +5,13 @@ T Add(T a, T b)
 {
 	return a + b;
 }
-
 int main()
 {
-	std::cout << Add(1,   2 ) << std::endl;
-	std::cout << Add(1.1, 2.1) << std::endl;
+	std::cout << Add(1,   2 ) << std::endl;	// 3
+	std::cout << Add(1.1, 2.1) << std::endl;// 3.2
+
+	std::cout << Add(1.1, 2) << std::endl; // error. T타입을
+										// 추론할수 없음.
+	std::cout << Add<double>(1.1, 2) << std::endl; // ok
 }
 
--- DAY1/5_함수템플릿2.cpp
+++ DAY1/5_함수템플릿2.cpp
@@ -2,14 +2,17 @@
 
 // 아래 코드를 보고 ? 채워 보세요
 template<typename T1, typename T2>
-? Add(T1 a, T2 b)
+//? Add(T1 a, T2 b)
+decltype(a + b) Add(T1 a, T2 b)
 {
 	return a + b;
 }
 
 int main()
 {
-	std::cout << Add(1.1, 2) << std::endl; 
+	std::cout << Add<double, int>(1.1, 2) << std::endl;  // 타입전달
+
+	std::cout << Add(1.1, 2) << std::endl; // 타입 추론
 
 }
 
