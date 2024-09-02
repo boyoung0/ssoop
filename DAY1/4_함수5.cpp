494eb616c004d59048600f2c56544119dffb6c12
ecourse <smkang@codenuri.co.kr>
2024-09-02 14:09:25 +0900

    Add files via upload

--- DAY1/4_함수5.cpp
+++ DAY1/4_함수5.cpp
@@ -9,6 +9,8 @@ int square1(int a)
 // C++11 에서 나온 새로운 함수 모양
 // => 후위 반환 타입(suffix return type)
 // => square2 는 square1 과 완전히 동일
+// => swift, rust, kotlin등의 대부분의 언어가 반환 타입을 뒤에 표기
+//    python 도 annotation 사용할때 뒤에 표기
 auto square2(int a) -> int
 {
 	return a * a;
